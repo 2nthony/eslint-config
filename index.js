550b01f76f0ce2ae7f8b7ae07944d27a2117be8f
2nthony <ant.hu@outlook.com>
2023-09-02 14:11:46 +0800

    feat: newline after var & brace-style disallow single line

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const { isPackageExists } = require('local-pkg')
+
+const TS = isPackageExists('typescript')
+
 module.exports = {
-  extends: '@antfu',
+  extends: [
+    '@antfu',
+    TS && './typescript',
+  ].filter(Boolean),
+
   rules: {
     'vue/prefer-separate-static-class': 'off',
+
+    // basic
+    'newline-after-var': ['error', 'always'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
 
     // antfu
     'antfu/if-newline': 'off',
---
+++ typescript.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rules: {
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+  },
+}
