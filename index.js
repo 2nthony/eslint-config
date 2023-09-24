43bda94245a46755b706a6fcc61a6df23b19bf55
2nthony <ant.hu@outlook.com>
2023-09-25 02:08:16 +0800

    fix: user configs types

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const VUE = isPackageExists('vue')
 
 /**
  * @typedef {import('eslint-define-config').FlatESLintConfigItem} FlatESLintConfigItem
- * @type {(userConfigs: FlatESLintConfigItem[]) => FlatESLintConfigItem[]}
+ * @type {(...userConfigs: FlatESLintConfigItem[]) => FlatESLintConfigItem[]}
  */
-exports._2nthony = userConfigs => antfu(
+exports._2nthony = (...userConfigs) => antfu(
   require('./configs/antfu'),
   require('./configs/base'),
   require('./configs/plugins/unused-imports'),
