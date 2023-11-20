c1191d4d29df9f7283c47e265c9c775c69208fa4
2nthony <ant.hu@outlook.com>
2023-11-21 02:22:03 +0800

    fix: upstream breaking changes

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const { VUE, REACT, TS } = require('./configs/_shared')
  */
 exports._2nthony = (...userConfigs) => antfu(
   ...require('./configs/antfu'),
-  require('./configs/base'),
-  require('./configs/plugins/unused-imports'),
-  TS && require('./configs/typescript'),
-  REACT && require('./configs/react'),
-  VUE && require('./configs/vue'),
-  userConfigs,
-).filter(Boolean)
+  ...[
+    require('./configs/base'),
+    require('./configs/plugins/unused-imports'),
+    TS && require('./configs/typescript'),
+    REACT && require('./configs/react'),
+    VUE && require('./configs/vue'),
+    userConfigs,
+  ].filter(Boolean),
+)
