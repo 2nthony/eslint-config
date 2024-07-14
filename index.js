e4e4eb5852c108e006d3329d6135a55f437b0b3d
2nthony <ant.hu@outlook.com>
2024-07-14 17:56:11 +0800

    fix: entry file

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const { VUE, REACT, TS } = require('./configs/_shared')
  */
 exports._2nthony = (...userConfigs) => antfu(
   ...require('./configs/antfu'),
+  require('./configs/base'),
+  require('./configs/plugins/unused-imports'),
   ...[
-    require('./configs/base'),
-    require('./configs/plugins/unused-imports'),
     TS && require('./configs/typescript'),
     REACT && require('./configs/react'),
     VUE && require('./configs/vue'),
-    ...userConfigs,
   ].filter(Boolean),
+  ...userConfigs,
 )
